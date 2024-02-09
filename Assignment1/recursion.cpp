8f55617a91c6f287c1d72e95f5c985d7150d6a2d
rajkumar963 <rajkumarsaw597@gmail.com>
2024-02-09 19:53:24 +0530

    new commit

---
+++ Assignment1/recursion.cpp
@@ -0,0 +1,83 @@
+//1. Write a recursive function to calculate the factorial of a number.
+// #include<iostream>
+// using namespace std;
+// int fact(int n){
+//     if(n==0){
+//         return 1;
+//     }
+//     return n*fact(n-1);
+// }
+// int main(){
+//     int n;
+//     cin>>n;
+//    fact(n);
+//     cout<<fact(n);
+//     return 0;
+// }
+
+
+//2. Write a recursive function to print numbers from n to 1.
+// #include<iostream>
+// using namespace std;
+// int print(int n){
+//     if(n==0){
+//         return 0;
+//     }
+//     print(n-1);
+//     cout<<n<<endl;
+// }
+// int main(){
+//     int n;
+//     cin>>n;
+//     print(n);
+//     return 0;
+// }
+
+
+//3. Write a recursive function to reverse a string.
+// #include<iostream>
+// using namespace std;
+// int reverse(string str){
+//     if(str.length()==0){
+//         return 0;
+//     }
+//     reverse(str.substr(1));
+//     cout<<str[0];
+// }
+// int main(){
+//     string str;
+//     cin>>str;
+//     reverse(str);
+// }
+
+
+//4. Write a recursive function to find the nth fibonacci number.
+// #include<iostream>
+// using namespace std;
+// int fibonacci(int n){
+//     int a=0,b=1,c;
+//     // int sum=0;
+//     if(n==0){
+//         cout<<"0";
+//     }
+//     if(n==1){
+//         cout<<"1";
+//     }else{
+//         for(int i=1;i<=n;i++){
+//         c=a+b;
+//         a=b;
+//         b=c;
+//     }
+//      cout<<"fibonacci("<<n<<") = "<<c;
+//     }
+// }
+// int main(){
+//     int n;
+//     cin>>n;
+//     fibonacci(n);
+//     return 0;
+//}
+
+
+
+#include<iostream>
--- hakerrank/1.c++
+++ hakerrank/1.c++
@@ -1,41 +1,63 @@
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     if(n==1)
+//     {cout<<"one";}
+//     if(n==2)
+//     {cout<<"two";}
+//     if(n==3)
+//     {cout<<"three";}
+//     if(n==4)
+//     {cout<<"four";}
+//     if(n==5)
+//     {cout<<"five";}
+//     if(n==6)
+//     {cout<<"six";}
+//     if(n==7)
+//     {cout<<"seven";}
+//     if(n==8)
+//     {cout<<"eight";}
+//     if(n==9)
+//     {cout<<"nine";}
+//         if(n>9)
+//     {
+//         cout<<"Greater than 9";
+//     }    
+//     }
+
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int x,y;
+//     cin>>x>>y;
+// 	if(x<y){
+// 	    cout<<"YES";
+// 	}
+// 	else{
+// 	    cout<<"NO";
+// 	}
+// 	return 0;
+// }
+
+
+#include <iostream>
 using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    if(n==1)
-    {cout<<"one";}
-    if(n==2)
-    {cout<<"two";}
-    if(n==3)
-    {cout<<"three";}
-    if(n==4)
-    {cout<<"four";}
-    if(n==5)
-    {cout<<"five";}
-    if(n==6)
-    {cout<<"six";}
-    if(n==7)
-    {cout<<"seven";}
-    if(n==8)
-    {cout<<"eight";}
-    if(n==9)
-    {cout<<"nine";}
-        if(n>9)
-    {
-        cout<<"Greater than 9";
-    }    
-    }
 
-
-
-#include<iostream>
-using namespace std;
-int main(){
-    int a,b;
-    cin>>a>>b;
-    if(a<b){
-        
-    }
+int main() {
+    int x,y;
+    cin>>x>>y;
+	// your code goes here
+	if(x*2>y){
+	    cout<<"YES";
+	}
+	else{
+	    cout<<"NO";
+	}
+	return 0;
 }
- 
