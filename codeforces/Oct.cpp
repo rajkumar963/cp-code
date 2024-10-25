26764cb9c376fcc2ccd9b966e2079f912d018432
rajkumar963 <rajkumarsaw597@gmail.com>
2024-10-25 18:30:45 +0530

    code update:

--- codeforces/Oct.cpp
+++ codeforces/Oct.cpp
@@ -54,28 +54,95 @@
 
 
 //Max Plus Size
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        vector<int>a(n);
-        for(int i=0;i<n;i++){
-            cin>>a[i];
-        }
-        int sum1=0,sum2=0,m1=0,m2=0;
-        for(int i=0;i<n;i+=2){
-            sum1++;
-            m1=max(m1,a[i]);
-        }
-        for(int i=1;i<n;i+=2){
-            sum2++;
-            m2=max(m2,a[i]);
-        }
-        cout<<max(sum1+m1,sum2+m2)<<endl;
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int t;
+//     cin>>t;
+//     while(t--){
+//         int n;
+//         cin>>n;
+//         vector<int>a(n);
+//         for(int i=0;i<n;i++){
+//             cin>>a[i];
+//         }
+//         int sum1=0,sum2=0,m1=0,m2=0;
+//         for(int i=0;i<n;i+=2){
+//             sum1++;
+//             m1=max(m1,a[i]);
+//         }
+//         for(int i=1;i<n;i+=2){
+//             sum2++;
+//             m2=max(m2,a[i]);
+//         }
+//         cout<<max(sum1+m1,sum2+m2)<<endl;
+//     }
+// }
 
-    }
-}
+
+//. Two Screens
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int q;
+//     cin>>q;
+//     while(q--){
+//         string s,t;
+//         cin>>s>>t;
+//         int ans=0;
+//         int len=min(s.length(),t.length());
+//         for(int i=0;i<len;i++){
+//             if(s[i]==t[i]){
+//                 ans++;
+//             }else{
+//                 break;
+//             }
+//         }
+//         if(ans>0){
+//             int x=t.size()-ans+1;
+//             cout<<x+s.size()<<endl;
+//         }
+//         else{
+//           cout<<s.size()+t.size()<<endl;
+//         }      
+//     }
+// }
+
+
+// //A. Sakurako and Kosuke
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int t;
+//     cin>>t;
+//     while(t--){
+//         int n;
+//         cin>>n;
+//         if(n%2==0){
+//             cout<<"Sakurako\n";
+//         }else{
+//             cout<<"Kosuke \n";
+//         }
+//     }
+// }
+
+
+// //A. Profitable Interest Rate
+// #include<bits/stdc++.h>
+// using namespace std;
+//   int main(){
+//     int t;
+//     cin>>t;
+//     while(t--){
+//         int a,b;
+//         cin>>a>>b;
+//         if(a>=b){
+//             cout<<a<<endl;
+//         }else if(a*2<=b){
+//             cout<<0<<endl;
+//         }
+//         else{
+//             cout<<2*a-b<<endl;
+//         }
+//     }
+// }
